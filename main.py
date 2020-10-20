df47b23ffd77ea131e179322625e8dbeb8bc5e59
Ira <lusy.neama@gmail.com>
2020-10-20 23:15:30 +0300

    heeeyyo-testing
    Signed-off-by: Ira <lusy.neama@gmail.com>

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from capture_window import get_window, get_image_
 
-exe_name = 'Discord.exe'  # it is Case sensitive
+exe_name = 'Calculator.exe'    # script will search for windows of this process (it is Case sensitive)
+run = 1                     # this boolean tells should the program continue to search
+wrong_pids = []             # list of processed PIDs with no window to show or/and windows that were already shown
 
-run = 1
-wrong_pids = []
-while (run):
+
+while run:
     print("Trying to find a window..")
-    curr_window_hwnd, wrong_pids = get_window(exe_name, wrong_pids)  # curr_window - will be shown
-                                                        # wrong_pids - list of pocessed PIDs with no window to show
-    if curr_window_hwnd:
+    curr_window_hwnd, wrong_pids = get_window(exe_name, wrong_pids)  # tries to find a new window handle by process name
+
+    if curr_window_hwnd: # if window handle was found
         # print("|DEBUG| wrong_pids: ",wrong_pids)
         # print("|DEBUG| curr_window_hwnd: ", curr_window_hwnd)
-        print("PID: ", wrong_pids[-1])
+        print("PID: ", wrong_pids[-1]) # writes a PID of the window that will be shown
         print("----Window was found!---- \n | Press ESC to shut down the script. \n | Press ENTER to find the next window.\n_______________\n")
-        run = get_image_(curr_window_hwnd)
+        run = get_image_(curr_window_hwnd)  # gets the image from this window to show
     else:
         print("Sorry, No window was found")
         run = 0
