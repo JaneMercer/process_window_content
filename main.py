dacb070241303bd16f90606c77011aeafe8a2ca8
Ira <lusy.neama@gmail.com>
2020-10-21 22:15:29 +0300

    runs faster, finds all window handlers right away
    Signed-off-by: Ira <lusy.neama@gmail.com>

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
-from capture_window import get_window, get_image_
+from capture_window import get_image_, get_window
 
-exe_name = 'Calculator.exe'    # script will search for windows of this process (it is Case sensitive)
-run = 1                     # this boolean tells should the program continue to search
-wrong_pids = []             # list of processed PIDs with no window to show or/and windows that were already shown
+exe_name = 'Telegram.exe'  # Name of the process for which this script will search for windows
+# run = 1  # bool - should the program continue to search
+wrong_pids = []  # list of processed PIDs with no window to show or/and windows that were already shown
+prev_foundPIDs = []
 
+print("Trying to find a window..")
+window_HWNDs, all_PIDs = get_window(exe_name)  # tries to find a new window handle by process name
 
-while run:
-    print("Trying to find a window..")
-    curr_window_hwnd, wrong_pids = get_window(exe_name, wrong_pids)  # tries to find a new window handle by process name
-
-    if curr_window_hwnd: # if window handle was found
-        # print("|DEBUG| wrong_pids: ",wrong_pids)
-        # print("|DEBUG| curr_window_hwnd: ", curr_window_hwnd)
-        print("PID: ", wrong_pids[-1]) # writes a PID of the window that will be shown
-        print("----Window was found!---- \n | Press ESC to shut down the script. \n | Press ENTER to find the next window.\n_______________\n")
+if window_HWNDs:  # if window handle was found
+    print("All process PID: ", all_PIDs)  # writes a PID of the windows that will be shown
+    print("All window HWNDs: ", window_HWNDs)
+    print("----Window was found!---- \n "
+          "| Press ESC to shut down the script. \n "
+          "| Press ENTER to find the next window.\n_______________\n")
+    for curr_window_hwnd in window_HWNDs:
         run = get_image_(curr_window_hwnd)  # gets the image from this window to show
-    else:
-        print("Sorry, No window was found")
-        run = 0
-#heeeyoo
+        if not run: break
+
+else:
+    print("Sorry, No window was found")
+    run = 0
+
--- venv/capture_window.py
+++ venv/capture_window.py
@@ -7,6 +7,7 @@ import numpy as np
 import cv2
 import time
 
+
 def enum_window_callback(hwnd, pid_and_windows):
     pid, windows = pid_and_windows
     tid, current_pid = win32process.GetWindowThreadProcessId(hwnd)
@@ -14,51 +15,49 @@ def enum_window_callback(hwnd, pid_and_windows):
         windows.append(hwnd)
 
 
-def get_window(w_name, wrong_processes):
+def get_window(w_name):
     try:
-        windows = []
         instansess = [item for item in psutil.process_iter() if item.name() == w_name]
-        # print(instansess)
-        windows.clear() #TO FIND A NEW WINDOW
-        # Просто pid первого попавшегося процесса с именем файла
-        if (instansess):
-            while not windows:
-                pid = next(item for item in psutil.process_iter() if (item.name() == w_name and item.pid not in wrong_processes) ).pid
-                # (вызовет исключение StopIteration, если не запущен)
-                # print("PID: ", pid)
-                win32gui.EnumWindows(enum_window_callback, [pid, windows])
-                wrong_processes.append(pid)
-            return (windows[0], wrong_processes)
+        _PIDs = [item.pid for item in instansess]
+
+        if instansess:
+            window_HWNDs = []
+
+            for pid in _PIDs:
+                win32gui.EnumWindows(enum_window_callback, [pid, window_HWNDs])
+
+            return (window_HWNDs, _PIDs)
         else:
-            # print("Process ", w_name, " was not found.")
-            return ([],wrong_processes)
+            print("Process ", w_name, " was not found.")
+            return ([], [])
 
     except Exception as e:
-        # if e == 'StopIteration':
-            print("No more windows. Bye-bye!")
-            return ([],wrong_processes)
-        # else: print(e)
+        print(e)
+        return ([], [])
 
 
-
-
-def resize(im):
-    width = int(im.shape[1] * 30 / 100)
-    height = int(im.shape[0] * 30 / 100)
+def resize(im, coef):  # resizes the image by coef
+    MAX_SIZE = 2000
+    MIN_SIZE = 10
+    width = int(im.shape[1] * coef)
+    height = int(im.shape[0] * coef)
     dsize = (width, height)
-    resized = cv2.resize(im, dsize)
-    return resized
+
+    if width > MIN_SIZE or height > MIN_SIZE or height < MAX_SIZE or width < MAX_SIZE:
+        return cv2.resize(im, dsize)
+    else:
+        print("Resized image is to small or to big. No resizing was done.")
+        return im
 
 
-def process_image(im):
-    image2 = resize(im)
+def process_image(im):  # processes the image the way you want
+    image2 = resize(im, 0.3)
     hsl = cv2.cvtColor(image2, cv2.COLOR_BGR2HLS)
-    mask = cv2.inRange(hsl, np.array([120,0,40]), np.array([135,40,255]))
+    mask = cv2.inRange(hsl, np.array([120, 0, 40]), np.array([135, 40, 255])) #masks specific color range
     blank_image = image2.copy()
     blank_image[:] = (0, 0, 255)
-    res = cv2.bitwise_and(blank_image,blank_image, mask= mask)
+    res = cv2.bitwise_and(blank_image, blank_image, mask=mask) #applies mask to the image and shows result on black bg
     return res
-    # return im
 
 
 def win_create_bitmap(window, w, h):
@@ -87,9 +86,9 @@ def win_create_bitmap(window, w, h):
     win32gui.ReleaseDC(window, hwndDC)
 
     if result:
-        return img,result
+        return img, result
     else:
-        return None,result
+        return None, result
 
 
 def get_image_(window):
@@ -100,11 +99,12 @@ def get_image_(window):
 
     if (w > 0 and h > 0):
         try:
-            while(1):
-                win_img, bool_img = win_create_bitmap(window, w,h)
+            while (1):
+                win_img, bool_img = win_create_bitmap(window, w, h)
                 if bool_img:  # PrintWindow Succeeded
                     cv_img = cv2.cvtColor(win_img, cv2.COLOR_RGBA2RGB)
-                    cv2.imshow('screen',process_image(cv_img))
+                    # cv2.imshow('screen',process_image(cv_img))
+                    cv2.imshow('screen', cv_img)
                     time.sleep(1.0 / 10)
                     k = cv2.waitKey(33)
                     if k == 27:  # Esc key to stop
@@ -124,5 +124,5 @@ def get_image_(window):
             print(e)
             return 1
     else:
-            print(" ... Ops!Window to small. \n")
-            return 1
+        print(" ... Ops!Window to small. \n")
+        return 1
